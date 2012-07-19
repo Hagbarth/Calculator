633bd388489ab9e3c7fc7b092fb07994de2b8dfc
Emil Hagbarth <emilhagbarth@Emils-MacBook-Pro.local>
2012-07-19 16:06:50 +0200

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Emil Rasmussen on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
